a7199ddaf8eebe8afe7a9bdebf1731f85e1a41dc
Diana <107621027+Dinka17@users.noreply.github.com>
2023-10-11 22:25:58 +0300

    create edit

--- src/SweetCreativity.WebApp/Controllers/ListingController.cs
+++ src/SweetCreativity.WebApp/Controllers/ListingController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NuGet.Protocol.Core.Types;
+using SweetCreativity.Core.Context;
 using SweetCreativity.Core.Entities;
 using SweetCreativity.Reposotories.Interfaces;
 using SweetCreativity.Reposotories.Repos;
@@ -10,13 +11,13 @@ namespace SweetCreativity.WebApp.Controllers
 {
     public class ListingController : Controller
     {
-        private  readonly IListingReposotory listingReposotory;
-        
-            
-         public ListingController (IListingReposotory listingReposotory)
-         {
+        private readonly IListingReposotory listingReposotory;
+
+
+        public ListingController(IListingReposotory listingReposotory)
+        {
             this.listingReposotory = listingReposotory;
-         }
+        }
         public IActionResult Index()
         {
             return View(listingReposotory.GetAll());
@@ -37,12 +38,72 @@ namespace SweetCreativity.WebApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                
+
                 listingReposotory.Add(item);
                 //listingReposotory.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
+            return View(item); 
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Listing item = listingReposotory.Get(id); // Отримуємо елемент за його ID
+            if (item == null)
+            {
+                return NotFound(); // Перевіряємо, чи знайдено елемент
+            }
+
             return View(item);
         }
+
+        [HttpPost]
+        public IActionResult Edit(Listing item)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Отримайте запис для оновлення з репозиторію (замість контексту бази даних)
+                    var existingItem = listingReposotory.Get(item.Id);
+
+                    if (existingItem != null)
+                    {
+                        // Оновіть дані, які ви хочете змінити
+                        existingItem.Title = item.Title;
+                        existingItem.Description = item.Description;
+                        existingItem.Product = item.Product;
+                        existingItem.CreatedAtListing = item.CreatedAtListing;
+                        existingItem.Location = item.Location;
+                        existingItem.Price = item.Price;
+                        existingItem.Weight = item.Weight;
+                        // і т.д. для інших полів
+
+                        // Збережіть зміни в репозиторії
+                        listingReposotory.Update(existingItem);
+
+                        return RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        // Якщо запис не знайдено, обробіть це відповідним чином
+                        return NotFound();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Обробте помилку при оновленні даних, якщо вона виникла
+                    // Виведіть або збережіть повідомлення про помилку для подальшого аналізу
+                    // Ви можете також відправити користувачеві повідомлення про помилку, якщо це потрібно
+                    return View(item);
+                }
+            }
+            return View(item);
+        }
+
     }
+
 }
+
+
+
